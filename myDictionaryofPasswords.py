7511ef289601ad39a505b713ef888037932b9fa5
Andi Folgado <ac.folgado@gmail.com>
2020-02-14 21:35:50 +0000

    Creating my dictionary to store my passwords
    The idea is to store the passwords created by the password generator in this dictionary. The user will access their password by simply entering the name of the site/application they want the password for. It will then output the relevant password.

---
+++ myDictionaryofPasswords.py
@@ -0,0 +1,13 @@
+#Now lets store these passwords & usernames in a dictionary 
+#But lets start with storing the password against the place we needed that password. 
+
+my_dictionary_of_passwords = {}
+userName = input("What username did you choose (if applicable):")
+siteRequestingPassword = input("Whats the name of the website/application you needed this password for:")
+password = input("What is your password for this website/application?:")
+
+my_dictionary_of_passwords[siteRequestingPassword] = password
+
+print(my_dictionary_of_passwords)
+
+
